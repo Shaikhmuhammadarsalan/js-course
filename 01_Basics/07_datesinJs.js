1f1ffff4e58ef7a265605aff8d300c9469f3386a
Shaikhmuhammadarsalan <79082010+Shaikhmuhammadarsalan@users.noreply.github.com>
2024-12-12 10:21:35 +0000

    New JS Lectures

---
+++ 01_Basics/07_datesinJs.js
@@ -0,0 +1,43 @@
+let myDate = new Date()
+console.log(myDate);
+
+// toString
+//console.log("toString", myDate.toString());
+
+// toDateString
+//console.log("toDateString", myDate.toDateString());
+
+// toISOString
+//console.log("toISOString", myDate.toISOString());
+
+// toJSON
+//console.log("toJSON", myDate.toJSON());
+
+// toLocaleDateString
+//console.log("toLocaleDateString", myDate.toLocaleDateString());
+
+const currentDate = ("toLocaleDateString", myDate.toLocaleDateString());
+//console.log("Todays Date is", currentDate);
+
+// toLocaleTimeString
+//console.log("toLocaleTimeString", myDate.toLocaleTimeString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+
+//console.log(newDate.getMonth() + 1);
+//console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+// newDate.toLocaleString('default', {
+//     weekday: "long",
+    
+// })
+
+console.log(newDate);
